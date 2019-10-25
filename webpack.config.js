e1dc64e0b420a832ecd826ef672a9230dfd82ac5
sinaesmaeili <sina.esmaeili01@gmail.com>
2019-10-25 19:20:52 -0400

    Initial commit with webpack build

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'src'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
